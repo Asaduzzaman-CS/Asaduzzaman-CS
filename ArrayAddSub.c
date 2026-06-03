cc4ab8697eba9dbf37aa9e176e470f0675786d6e
Muhammad Asaduzzaman <islamrommon42@gmail.com>
2026-06-03 17:29:56 +0600

    Simple Matrix

---
+++ ArrayAddSub.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+int A[3][4],B[3][4];
+printf("Enter element for a matrix: ");
+for(i=0;i<3;i++)
+    for(j=0;j<4;j++)
+    printf("A[%d][%d]=",i,j);
+scanf("%d",A[i][j]);
+printf("\n");
+printf("A=");
+for(i=0;i<3;i++)
+printf("\t");
+    for(j=0;j<4;j++)
+    printf("%d",A[i][j]);
+printf("\n");
+
+printf("Enter element for B matrix: ");
+for(i=0;i<i;i++)
+    for(j=0;j<j;j++)
+printf("B[%d][%d]=",i,j);
+scanf("%d",&A[i][j]);
+printf("\n");
+printf("A=");
+for(i=0;i<3;i++)
+    printf("\t");
+    for(j=0;j<4;j++)
+        printf("%d",B[i][j]);
+    printf("\n");
+
+
+}
