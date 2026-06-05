030f7580f3c1fc37e292b4519f335a1fd443056e
Muhammad Asaduzzaman <islamrommon42@gmail.com>
2026-06-05 17:02:15 +0600

    Array Subtraction (Matrix)

---
+++ ArraySubtraction.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+int A[3][4],B[3][4],C[3][4];
+int i,j;
+
+printf("Enter A matrix: \n");
+for(i=0;i<3;i++)
+   for(j=0;j<4;j++)
+    scanf("%d",&A[i][j]);
+
+   printf("Enter B matrix: \n");
+   for(i=0;i<3;i++)
+    for(j=0;j<4;j++)
+    scanf("%d",&B[i][j]);
+
+   for(i=0;i<3;i++)
+    for(j=0;j<4;j++)
+{
+  C[i][j]=A[i][j]-B[i][j];
+}
+    printf("Subtraction Matrix:\n");
+
+for(i=0;i<3;i++)
+{
+    for(j=0;j<4;j++)
+    {
+        printf("%d ",C[i][j]);
+    }
+    printf("\n");
+}
+ return 0;
+}
+
