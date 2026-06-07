425e8a9dc205576944aae595cb9cbb83f3e7ce50
Muhammad Asaduzzaman <islamrommon42@gmail.com>
2026-06-08 00:32:06 +0600

    Refactor matrix transpose program for clarity

--- transposematrix.c
+++ transposematrix.c
@@ -1,34 +1,56 @@
 #include<stdio.h>
-int main(){
-int A[10][10],transpose[10][10],i,j,col,row;
-printf("Enter the number of row and columns for the matrix: ");
-scanf("%d %d", &row,&col);
-for(i=0;i<row;i++)
-    for(j=0;j<col;j++)
-    {
 
-    printf("A[%d][%d]= ",i,j);
-scanf("%d",&A[i][j]);
+int main()
+{
+    int A[10][10], transpose[10][10];
+    int row, col, i, j;
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    printf("\nEnter the elements of the matrix:\n");
+
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            printf("A[%d][%d] = ", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("\nEntered Matrix:\n");
+
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i = 0; i < row; i++)
+    {
+        for(j = 0; j < col; j++)
+        {
+            transpose[j][i] = A[i][j];
+        }
+    }
+
+    printf("\nTranspose Matrix:\n");
+
+    for(i = 0; i < col; i++)
+    {
+        for(j = 0; j < row; j++)
+        {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
-printf("\nEntered Matrix\n");
-for(i=0;i<row;i++)
-    for(j=0;j<col;j++)
-    {
-        transpose[j][i]=A[i][j];
-    }
-
-    {
-        printf("%d",A[i][j]);
-    }
-
-    printf("\n");
-    printf("\nTranspose matrix.\n");
-
-    for(i=0;i<col;i++){
-        for(j=0;j<row;j++)
-    {
-        printf("%d",transpose[i][j]);
-    }
     }
 
     }
