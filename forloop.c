6b18980188036700300fd2fe60310a0ffc29c39a
Muhammad Asaduzzaman <islamrommon42@gmail.com>
2026-06-15 00:59:42 +0600

    For loop Iteration program

---
+++ forloop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+int i,totalIteration;
+
+printf("Total number of Iteration: \n");
+scanf("%d",&totalIteration);
+
+for(i=0; i<=totalIteration; i++)
+    printf("%d. Hello Bangladesh\n",i);
+}
